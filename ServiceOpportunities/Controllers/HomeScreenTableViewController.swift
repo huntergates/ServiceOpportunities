00e1309553fb49966a2ef65170d6ddbf3a95270e
Hunter Gates <huntergates998@gmail.com>
2020-06-21 23:55:25 -0500

    tried to do segue programatically, didn't go well. Changed it back but appears to show double screen when running in simulator.

--- ServiceOpportunities/Controllers/HomeScreenTableViewController.swift
+++ ServiceOpportunities/Controllers/HomeScreenTableViewController.swift
@@ -4,10 +4,11 @@ import SnapKit
 
 class HomeScreenTableViewController: UITableViewController {
     
-    var companies = [Company(companyName: "TECHO", companyPhone: "(33) 3615 3258", distance: 5, address: "Something Road", image: UIImage(named: "teslacompany") ?? UIImage(), companyDescription: "They build houses."),
-        Company(companyName: "Galilea 2000 A.C.", companyPhone: "(33)3613 2496 / 0443310405808", distance: 6, address: "Yes Boulevard", image: UIImage(named: "teslacompany") ?? UIImage(), companyDescription: "They do things.")]
-    
     var companySelection: Company?
+    
+    //var services = CompanyServices()
+    
+    var companies = [Company(companyName: "TECHO", companyPhone: "(33) 3615 3258", distance: 5, address: "Something Road", image: UIImage(named: "teslacompany") ?? UIImage(), companyDescription: "They build houses."), Company(companyName: "Galilea 2000 A.C.", companyPhone: "(33)3613 2496 / 0443310405808", distance: 6, address: "Yes Boulevard", image: UIImage(named: "teslacompany") ?? UIImage(), companyDescription: "They do things.")]
     
     override func viewDidLoad() {
         self.tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
---
+++ ServiceOpportunities/Networking/CompanyServices.swift
@@ -0,0 +1,7 @@
+//import Foundation
+//import UIKit
+//
+//class CompanyServices {
+//    var companies = [Company(companyName: "TECHO", companyPhone: "(33) 3615 3258", distance: 5, address: "Something Road", image: UIImage(named: "teslacompany") ?? UIImage(), companyDescription: "They build houses."),
+//           Company(companyName: "Galilea 2000 A.C.", companyPhone: "(33)3613 2496 / 0443310405808", distance: 6, address: "Yes Boulevard", image: UIImage(named: "teslacompany") ?? UIImage(), companyDescription: "They do things.")]
+//}
